9b04a18d55710522dc6c2d89aab11004556759b8
Prakhar Varshney <prakharnegative@gmail.com>
2018-11-14 20:23:46 +0530

    Updated UI

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [  "Full Service.", "Web development.", "Connect To grow", "Hello World" ,"Amsterdam.",];
+  var dataText = [  "Need Services ?", "Mentorship ?", "Freelancer?", "One Platform" ,"Connect To grow",];
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
